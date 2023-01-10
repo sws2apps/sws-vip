057fa3f46a6c573fc1902d0aabdbafba4f8935a8
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-01-10 22:49:51 +0300

    fix(startup): handle invalid data from old cpe app, disallow some email domains

--- src/features/myAssignments/MyAssignmentsMonthItem.jsx
+++ src/features/myAssignments/MyAssignmentsMonthItem.jsx
@@ -6,12 +6,15 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { sourceLangState } from '../../states/main';
 import { t } from 'i18next';
+import { republishScheduleState } from '../../states/congregation';
 
 const MyAssignmentsMonthItem = ({ assignment }) => {
   const { weekOf, assignmentContent, assignmentName, assignmentSource, assignmentTime, studyPoint } = assignment;
 
   const sourceLang = useRecoilValue(sourceLangState);
-  const lang = sourceLang.toUpperCase();
+  const isRepublish = useRecoilValue(republishScheduleState);
+
+  const lang = sourceLang?.toUpperCase();
 
   const dateValue = weekOf.split('/')[1];
 
@@ -43,36 +46,48 @@ const MyAssignmentsMonthItem = ({ assignment }) => {
 
   return (
     <Box sx={{ display: 'flex', gap: '10px', alignItems: 'flex-start' }}>
-      <Paper
-        sx={{ backgroundColor: assignment.isBehalf ? deepOrange[900] : indigo[800], padding: '0 20px' }}
-        elevation={3}
-      >
-        <Typography align="center" sx={{ fontWeight: 'bold', fontSize: '22px', color: 'white' }}>
-          {dateValue}
-        </Typography>
-      </Paper>
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', gap: '8px' }}>
-        <Box
-          sx={{ flexGrow: 1, display: 'flex', alignItems: 'flex-start', gap: '10px', justifyContent: 'space-between' }}
-        >
-          <Typography variant="h6" sx={{ lineHeight: 1.2 }}>
-            {assignmentName}
-          </Typography>
-          {assignment.class && (
-            <Avatar sx={{ bgcolor: blue[500], width: '25px', height: '25px', fontSize: '15px', fontWeight: 'bold' }}>
-              {assignment.class}
-            </Avatar>
-          )}
-        </Box>
-        {assignment.isBehalf && (
-          <Typography sx={{ lineHeight: 1.2 }}>{`${t('presentedBy')}${assignment.personDispName}`}</Typography>
-        )}
-        {assignment.assignmentType === 'ayf' && <Typography sx={{ lineHeight: 1.2 }}>{getStuAYF()}</Typography>}
-        <Typography sx={{ lineHeight: 1.2 }}>{getSource()}</Typography>
-        {assignmentContent && assignmentContent[lang] !== '' && (
-          <Typography sx={{ lineHeight: 1.2, fontSize: '14px' }}>{assignmentContent[lang]}</Typography>
-        )}
-      </Box>
+      {!isRepublish && (
+        <>
+          <Paper
+            sx={{ backgroundColor: assignment.isBehalf ? deepOrange[900] : indigo[800], padding: '0 20px' }}
+            elevation={3}
+          >
+            <Typography align="center" sx={{ fontWeight: 'bold', fontSize: '22px', color: 'white' }}>
+              {dateValue}
+            </Typography>
+          </Paper>
+          <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', gap: '8px' }}>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: 'flex',
+                alignItems: 'flex-start',
+                gap: '10px',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Typography variant="h6" sx={{ lineHeight: 1.2 }}>
+                {assignmentName}
+              </Typography>
+              {assignment.class && (
+                <Avatar
+                  sx={{ bgcolor: blue[500], width: '25px', height: '25px', fontSize: '15px', fontWeight: 'bold' }}
+                >
+                  {assignment.class}
+                </Avatar>
+              )}
+            </Box>
+            {assignment.isBehalf && (
+              <Typography sx={{ lineHeight: 1.2 }}>{`${t('presentedBy')}${assignment.personDispName}`}</Typography>
+            )}
+            {assignment.assignmentType === 'ayf' && <Typography sx={{ lineHeight: 1.2 }}>{getStuAYF()}</Typography>}
+            <Typography sx={{ lineHeight: 1.2 }}>{getSource()}</Typography>
+            {assignmentContent && assignmentContent[lang] !== '' && (
+              <Typography sx={{ lineHeight: 1.2, fontSize: '14px' }}>{assignmentContent[lang]}</Typography>
+            )}
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
