b1581f3b079ee9c8e51c1277bfa5971d255190a6
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-25 19:27:36 +0300

    feat(app): add disclaimer for new address

--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-};
-
-initializeApp(firebaseConfig);
